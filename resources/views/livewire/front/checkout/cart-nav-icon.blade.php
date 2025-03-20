38146337a5965553f38b53bac538fecbac4e162e
tomashina <tomislav@agmedia.hr>
2025-03-20 11:56:00 +0100

    kosarica drop down

--- resources/views/livewire/front/checkout/cart-nav-icon.blade.php
+++ resources/views/livewire/front/checkout/cart-nav-icon.blade.php
@@ -1,6 +1,38 @@
-<div>
-    <button type="button" class="btn btn-icon fs-xl btn-outline-secondary position-relative border-0 rounded-circle animate-scale" data-bs-toggle="offcanvas" data-bs-target="#shoppingCart" aria-controls="shoppingCart" aria-label="Shopping cart">
+
+
+<div class="dropdown">
+    <button type="button" class="btn btn-icon fs-xl btn-outline-secondary position-relative border-0 rounded-circle animate-scale " data-bs-toggle="dropdown" data-bs-trigger="hover" aria-haspopup="true" aria-expanded="false">
         <span id="cart-header-count" class="position-absolute top-0 start-100 badge fs-xs text-bg-primary rounded-pill ms-n3 z-2" style="--cz-badge-padding-y: .25em; --cz-badge-padding-x: .42em">{{ $count }}</span>
         <i class="ci-shopping-cart animate-target"></i>
     </button>
+    <div class="dropdown-menu dropdown-menu-end">
+
+        <div class="widget widget-cart px-3 pt-2 pb-3" style="width: 24rem;">
+
+
+                <div class="widget-cart-item pb-2 border-bottom">
+
+                    <div class="d-flex align-items-center">
+                        <button type="button" aria-label="Remove" class="btn-close text-danger me-2"></button>
+                        <a href="#" class="d-block flex-shrink-0 pt-2">
+                            <img src="{{ asset('assets/images/auto-gume.jpeg') }}" alt="Continental 195/60 R15 AVANT 140M" title="Continental 195/60 R15 AVANT 140M" style="width: 5rem;"></a>
+                        <div class="ps-2">
+                            <h6 class="widget-product-title">
+                                <a href="#">Continental 195/60 R15 AVANT 140M</a>
+                            </h6>
+                            <div class="widget-product-meta"><span class="text-primary me-2">120,00 €</span><span class="text-muted">x 1</span></div>
+
+                        </div>
+                    </div>
+                </div>
+        </div>
+        <div class="d-flex flex-wrap justify-content-between px-3 align-items-center py-3">
+            <div class="fs-sm me-2 py-2">
+                <span class="text-muted">Ukupno:</span>
+                <span class="text-primary fs-base ms-1">6.64€</span>
+            </div>
+        </div>
+        <a href="https://www.zuzi.hr/kosarica" class="btn btn-primary btn-sm d-block px-3  w-100"><i class="ci-card me-2 fs-base align-middle"></i>Dovrši kupnju</a>
+
+        </div>
 </div>
