1a702a0531c9d03697a495bba44cadad837b6990
tomashina <tomislav@agmedia.hr>
2025-03-28 12:21:41 +0100

    200

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -218,7 +218,7 @@ class DashboardController extends Controller
 
                         $count++;
 
-                        if ($count > 500) {
+                        if ($count > 200) {
                             return redirect()->route('dashboard');
                         }
                     }
