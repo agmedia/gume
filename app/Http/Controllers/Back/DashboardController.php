95109077ba8cdf8f8a6ba218b08ef299231e41f3
tomashina <tomislav@agmedia.hr>
2025-03-31 10:51:21 +0200

    fixes front

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -218,7 +218,7 @@ class DashboardController extends Controller
 
                         $count++;
 
-                        if ($count > 30) {
+                        if ($count > 200) {
                             return redirect()->route('dashboard');
                         }
                     }
