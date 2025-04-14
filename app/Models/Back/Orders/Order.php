9016c50ff374667314f40b4b225c66ce53d866e8
tomashina <tomislav@agmedia.hr>
2025-04-14 19:40:35 +0200

    fixes backend reservations

--- app/Models/Back/Orders/Order.php
+++ app/Models/Back/Orders/Order.php
@@ -76,7 +76,7 @@ class Order extends Model
 
     public function reservation()
     {
-        return $this->hasOne(Reservation::class, 'order_id')->with('reservations');
+        return $this->hasOne(Reservation::class,  'id', 'reservation_id');
 
     }
 
