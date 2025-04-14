c6fe0cd34384eb9004f7f6ae9c817b1a3b1ba949
tomashina <tomislav@agmedia.hr>
2025-04-14 11:49:36 +0200

    kat fix

--- resources/views/front/layouts/widget/widget_product_carousel.blade.php
+++ resources/views/front/layouts/widget/widget_product_carousel.blade.php
@@ -17,10 +17,10 @@
     <div class="position-relative pb-xxl-3">
 
         <!-- External slider prev/next buttons visible on screens > 500px wide (sm breakpoint) -->
-        <button type="button" class="popular-prev btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-start position-absolute top-50 start-0 z-2 translate-middle mt-n5 d-none d-sm-inline-flex" aria-label="Prev">
+        <button type="button" class="popular-prev{{ str_replace(' ', '', $data['title']) }} btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-start position-absolute top-50 start-0 z-2 translate-middle mt-n5 d-none d-sm-inline-flex" aria-label="Prev">
             <i class="ci-chevron-left fs-lg animate-target"></i>
         </button>
-        <button type="button" class="popular-next btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-end position-absolute top-50 start-100 z-2 translate-middle mt-n5 d-none d-sm-inline-flex" aria-label="Next">
+        <button type="button" class="popular-next{{ str_replace(' ', '', $data['title']) }} btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-end position-absolute top-50 start-100 z-2 translate-middle mt-n5 d-none d-sm-inline-flex" aria-label="Next">
             <i class="ci-chevron-right fs-lg animate-target"></i>
         </button>
 
@@ -30,8 +30,8 @@
             "spaceBetween": 20,
             "loop": true,
             "navigation": {
-              "prevEl": ".popular-prev",
-              "nextEl": ".popular-next"
+              "prevEl": ".popular-prev{{ str_replace(' ', '', $data['title']) }}",
+              "nextEl": ".popular-next{{ str_replace(' ', '', $data['title']) }}"
             },
             "breakpoints": {
               "768": {
@@ -45,7 +45,7 @@
               }
             }
           }'>
-            <div class="swiper-wrapper">
+            <div class="swiper-wrapper" >
 
                 @foreach ($data['items'] as $product)
                     <!-- Product-->
@@ -59,10 +59,10 @@
 
     <!-- External slider prev/next buttons visible on screens < 500px wide (sm breakpoint) -->
     <div class="d-flex justify-content-center gap-2 mt-1 pt-4 d-sm-none">
-        <button type="button" class="popular-prev btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-start me-1" aria-label="Prev">
+        <button type="button" class="popular-prev{{ str_replace(' ', '', $data['title']) }} btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-start me-1" aria-label="Prev">
             <i class="ci-chevron-left fs-lg animate-target"></i>
         </button>
-        <button type="button" class="popular-next btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-end" aria-label="Next">
+        <button type="button" class="popular-next{{ str_replace(' ', '', $data['title']) }} btn btn-icon btn-outline-secondary bg-body rounded-circle animate-slide-end" aria-label="Next">
             <i class="ci-chevron-right fs-lg animate-target"></i>
         </button>
     </div>
