12cd612387d6d31d86c3a75f7e78afc38f06929e
tomashina <tomislav@agmedia.hr>
2025-04-14 16:20:40 +0200

    fixes

--- resources/views/front/catalog/product/index.blade.php
+++ resources/views/front/catalog/product/index.blade.php
@@ -171,6 +171,8 @@
 
                         <p class="fs-sm fw-normal mb-0">{{ $data->product->namjena }}</p>
 
+                        <p class="fs-sm fw-normal mb-2">Šifra: {{ $data->product->sku }}</p>
+
 
                         @if($data->product->sirina and $data->product->visina and $data->product->promjer)
                         <p><strong>{{ $data->product->sirina }}/{{ $data->product->visina }}  {{ $data->product->promjer }} {{ $data->product->nosivnost }}</strong></p>
