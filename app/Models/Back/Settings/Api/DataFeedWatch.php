fd8d0ebe4132010f084ad77fe2d1b77046d44129
Filip Jankoski <filip@agmedia.hr>
2025-04-29 12:39:22 +0200

    _added: Second URL to update CRON.

--- app/Models/Back/Settings/Api/DataFeedWatch.php
+++ app/Models/Back/Settings/Api/DataFeedWatch.php
@@ -13,6 +13,11 @@ class DataFeedWatch
     protected $url = 'https://feeds.datafeedwatch.com/70335/d2bfb7399e3bee04d0dabb9b5f0954de960f8569.xml';
 
     /**
+     * @var string
+     */
+    protected $url_2 = 'https://feeds.datafeedwatch.com/70335/d8aa73ceb924b75fd493399154b0c61f3ec93178.xml';
+
+    /**
      * @var \$1|false|\SimpleXMLElement|null
      */
     protected $feed = null;
@@ -57,6 +62,29 @@ class DataFeedWatch
             }
         }
 
-        return $count;
+        $count_2 = $this->checkSecondUrl();
+
+        return $count + $count_2;
+    }
+
+
+    /**
+     * Checks the second URL for a feed and updates product prices and quantities if available.
+     *
+     * Verifies if the second URL is not empty, attempts to load the feed data from the URL,
+     * and updates the products by invoking the `updatePricesAndQuantity` method. Returns the count
+     * of successfully updated products. If the second URL is empty, no updates are performed.
+     *
+     * @return int The number of products successfully updated or 0 if the second URL is empty.
+     */
+    public function checkSecondUrl(): int
+    {
+        if ($this->url_2 != '') {
+            $this->feed = simplexml_load_file($this->url_2);
+
+            return $this->updatePricesAndQuantity();
+        }
+
+        return 0;
     }
 }
