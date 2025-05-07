04cfb82a25960053cf4c5c913a78e72c62226963
tomashina <tomislav@agmedia.hr>
2025-05-07 18:09:53 +0200

    gogole

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -39,11 +39,11 @@
         @yield('google_data_layer')
         <!-- Google Tag Manager -->
             <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <!--  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-TKZGQZV');</script>
+                })(window,document,'script','dataLayer','#');</script> -->
             <!-- End Google Tag Manager -->
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -53,7 +53,7 @@
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', ' G-Q2GNBMK18T');
+            gtag('config', ' G-X8ZP5E2BYT');
         </script>
     @endif
 
