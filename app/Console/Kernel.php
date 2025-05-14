fee49d005e667dc659b0c63f0ad3990053d7089e
tomashina <tomislav@agmedia.hr>
2025-05-14 11:34:21 +0200

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:prices_and_quantity')->everyMinute();
+        $schedule->command('update:prices_and_quantity')->everySixHours();
     }
 
 
